908638eab26098765294942a63084451b30fd9e5
Anna Kizeeva <fenicemail1@gmail.com>
2021-02-23 00:33:06 +0300

    add junit4

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,7 +1,9 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,27 +16,27 @@ public class TestClass {
 
     protected WebDriver driver;
 
-    @BeforeEach
-    void setUp() throws MalformedURLException {
-//        String selenoidURL = "http://195.178.20.182:4444/wd/hub";
-//        DesiredCapabilities caps = new DesiredCapabilities();
-//        caps.setBrowserName("chrome");
-//        caps.setVersion("88.0");
-//        caps.setCapability("enableVNC", true);
-//        caps.setCapability("screenResolution", "1920x1080");
-//        caps.setCapability("enableVideo", false);
-//        caps.setCapability("enableLog", true);
-//
-//        driver = new RemoteWebDriver(new URL(selenoidURL), caps);
+    @Before
+    public void setUp() throws MalformedURLException {
+        String selenoidURL = "http://localhost:4444/wd/hub";
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("chrome");
+        caps.setVersion("88.0");
+        caps.setCapability("enableVNC", true);
+        caps.setCapability("screenResolution", "1920x1080");
+        caps.setCapability("enableVideo", false);
+        caps.setCapability("enableLog", true);
 
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driver = new RemoteWebDriver(new URL(selenoidURL), caps);
+
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
 
     }
 
     @Test
-    void openPage() {
+    public void openPage() {
         driver.get("https://otus.ru/");
         String title = driver.getTitle();
         assertEquals(title, "Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям");
@@ -42,8 +44,8 @@ public class TestClass {
 
     }
 
-    @AfterEach
-    void setDown() {
+    @After
+    public void setDown() {
         if (driver != null) {
             driver.quit();
         }
